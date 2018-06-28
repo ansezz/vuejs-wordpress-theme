4bd946dee734972d0eddf4a1ff0dfa1d0dc884f0
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2018-06-28 16:43:51 +0100

    change default tmp dir

--- deploy/deploy/config/deploy/test.rb
+++ deploy/deploy/config/deploy/test.rb
@@ -9,5 +9,7 @@ role :server, %w{138.68.253.178}
 
 set :deploy_to, '/home/laravel-vuejs/domains/dev.laravel-vuejs.com/public_html'
 
+set :tmp_dir, '/home/laravel-vuejs/tmp'
+
 set :linked_files, %w{dist/pwa-mat/.htaccess dist/pwa-mat/.htpasswd}
 
