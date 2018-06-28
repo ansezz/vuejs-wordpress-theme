42cba8895ced625739c0d0c8fa3a248e99f4bcf5
Anass Ez-Zouaine <ans-ezzouaine@hotmail.com>
2018-06-28 17:24:24 +0100

    refactor deploy system

--- deploy/deploy/config/deploy/test.rb
+++ deploy/deploy/config/deploy/test.rb
@@ -11,7 +11,7 @@ server '138.68.253.178', user: fetch(:user), roles: %w{app}
 
 role :server, %w{138.68.253.178}
 
-set :deploy_to, '/home/laravel-vuejs/domains/dev.laravel-vuejs.com/public_html'
+set :deploy_to, '/home/#{fetch(:user)}/domains/#{fetch(:domain)}/public_html'
 
 set :tmp_dir, '/home/laravel-vuejs/tmp'
 
